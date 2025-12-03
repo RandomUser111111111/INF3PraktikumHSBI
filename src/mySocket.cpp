5f26823339c4d2279edecb1d8d821d0c96fadc05
olbrfh <oliver.brixel@hsbi.de>
2025-12-03 17:35:30 +0100

    new myResponse

--- src/mySocket.cpp
+++ src/mySocket.cpp
@@ -5,35 +5,60 @@ using namespace std;
 string myTCPServer::myResponse(string input){
     // input = "2 4" [x y]
     
-    int x,y;
+    int x,y = 0;
 
-    istringstream iss(input);
-    iss >> x >> y;
+    if(input.compare(0, 4, "INIT")== 0){
+        initWorld();
+        MOVES = 0;
 
-    cout << "x: " << x << " y: " << y << endl;
-
-    TASK3::ShootResult tmp = this->world.shoot(x, y);
-
-    if(tmp == TASK3::WATER){
-        MOVES++;
-        return "WATER\n";
-    } if(tmp == TASK3::SHIP_HIT){
-        //POINTS++;
-        MOVES++;
-        return "SHIP HIT\n";
-    } if(tmp == TASK3::SHIP_DESTROYED){
-        //POINTS++;
-        MOVES++;
-        return "SHIP DESTROYED\n";
-    } if(tmp == TASK3::ALL_SHIPS_DESTROYED){
-        //POINTS++;
-        MOVES++;
-        return "ALL SHIPS DESTROYED\n";
-    } if(tmp == TASK3::GAME_OVER){
-        return "GAME OVER\n";
+        return "OK";
     }
 
-    return iss.str();
+    if(input.compare(0, 6, "COORD[") == 0){
+        int endInt; // Laenge des Input
+        int comma;  // Position des Kommas
+
+        comma = input.find(',');
+        endInt = input.find(']');
+
+        string strX = input.substr(6, comma - (6));
+        string strY = input.substr(comma+1, endInt - (comma + 1));
+
+        x = stoi(strX);
+        y = stoi(strY);
+
+        cout << "x: " << x << " y: " << y << endl;
+
+        TASK3::ShootResult tmp = this->world.shoot(x, y);
+
+        if(tmp == TASK3::WATER){
+            MOVES++;
+            return "WATER\n";
+        } if(tmp == TASK3::SHIP_HIT){
+            //POINTS++;
+            MOVES++;
+            return "SHIP HIT\n";
+        } if(tmp == TASK3::SHIP_DESTROYED){
+            //POINTS++;
+            MOVES++;
+            //test
+            return "SHIP DESTROYED\n";
+        } if(tmp == TASK3::ALL_SHIPS_DESTROYED){
+            //POINTS++;
+            MOVES++;
+            return "ALL SHIPS DESTROYED\n";
+        } if(tmp == TASK3::GAME_OVER){
+            return "GAME OVER\n";
+        }
+
+        return "ERROR\n";
+    }
+
+    else {
+        return "UNKNOWN COMMAND\n";
+    }
+
+    return "ERROR\n";
 }
 
 int myTCPServer::getPoints(){
@@ -46,11 +71,12 @@ void myTCPServer::setPoints(int points_){
 
 void myTCPServer::initWorld(){
     this->world = TASK3::World();
+    printWorld();
 }
 
 void myTCPServer::initWorld(int maxX, int maxY, int nmbFiver, int nmbFourer, int nmbThreer, int nmbTwoer){
-    
     this->world = TASK3::World(maxX, maxY, nmbFiver, nmbFourer, nmbThreer, nmbTwoer);
+    printWorld();
 }
 
 int myTCPServer::getMoves(){
